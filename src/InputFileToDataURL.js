c2b61a5704d5744e1f15f1816d7b3ac7d24c82e3
Vicente Jr Yuchitcho <chitchu@gmail.com>
2016-09-08 09:51:12 +1000

    add react-filetodataurl

---
+++ src/InputFileToDataURL.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class InputFileToDataURL extends Component {
+  constructor() {
+    super();
+    this.handleFileChange = this.handleFileChange.bind(this);
+  }
+
+  handleFileChange({target: {files: [file]}}) {
+    const reader = new FileReader();
+    reader.onload = ({target: {result}}) => {
+      this.props.onChange(result);
+    };
+    reader.readAsDataURL(file);
+  }
+  render() {
+    return <input type='file' onChange={this.handleFileChange} accept='image/*' />
+  }
+}
+
+
+export {
+  InputFileToDataURL as default
+}
