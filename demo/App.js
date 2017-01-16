5ba83631c10e04d08da2d4eeab8a070409445063
Vicente Jr Yuchitcho <chitchu@gmail.com>
2017-01-16 16:22:47 +1100

    :sparkles: Configurable demo

--- demo/App.js
+++ demo/App.js
@@ -6,6 +6,8 @@ import Mosaic from '../src/Mosaic';
 
 import debounce from 'lodash/debounce';
 
+import settingsIcons from './ic_settings_black_24px.svg';
+
 class App extends Component {
 
   state = {
@@ -24,7 +26,7 @@ class App extends Component {
     display: flex;
     justify-content: center;
     align-items: center;
-  `;
+  `
 
   Input = Style.input`
     padding: 1rem;
@@ -50,12 +52,14 @@ class App extends Component {
     &:-moz-placeholder { /* Firefox 18- */
       color: pink;
     }
-  `;
+  `
 
   StyledMosaic = Style(Mosaic)`
     position: absolute;
     z-index: -1;
-  `;
+  `
+
+
 
   componentDidMount() {
     this.setState({
@@ -96,17 +100,200 @@ class App extends Component {
     }
   }
 
-  handleTileRenderer = ({x, y, ...rest}) => (
-    <circle cx={x} cy={y} r={this.state.tileSize / 2} {...rest} />
-  );
+  // Render an alternating triangle
+  polygonRenderer = ({x, y, tileSize, key, ...rest}) => (
+    <polygon key={key} points={key % 2 ?  `${x},${y} ${x+tileSize},${y} ${x+tileSize/2},${y+tileSize}` : `${x},${y+tileSize} ${x+tileSize/2},${y} ${x+tileSize},${y+tileSize}`} {...rest} />
+  )
+
+  circleRenderer = ({x, y, tileSize, ...rest}) => (
+    <circle cx={x} cy={y} r={tileSize / 2} {...rest} />
+  )
+
+  handleOptionsChange = ({tileSize, selectedRenderer}) => {
+    this.setState({
+      tileSize: tileSize,
+      tileRenderer: selectedRenderer
+    });
+  }
+
+  emitter = {
+    on(handler) {
+      this.handler = handler;
+    },
+    fire(args) {
+      if (this.handler) this.handler(args);
+    }
+  }
+
+  handleProgress = ({current, total}) => {
+    const progress = (current / total) * 100;
+    this.emitter.fire({progress});
+  }
 
   render () {
     return (
       <this.Container>
+        <Progress emitter={this.emitter} />
+        <Options onChange={this.handleOptionsChange} tileSize={this.state.tileSize} renderers={['circleRenderer', 'polygonRenderer']} />
         <this.Input type="text" placeholder="Enter image url" autoFocus onChange={this.handleOnChange} />
-        <Mosaic src={this.state.image} tileSize={this.state.tileSize} width={this.state.mosaicWidth} height={this.state.mosaicHeight} tileRenderer={this.handleTileRenderer} style={{position:'absolute', zIndex:-1, left: 0, top: 0}}/>
+        <Mosaic src={this.state.image} tileSize={this.state.tileSize} width={this.state.mosaicWidth} height={this.state.mosaicHeight} tileRenderer={this[this.state.tileRenderer] || null} onProgress={this.handleProgress} style={{position:'absolute', zIndex:-1, left: 0, top: 0}}/>
       </this.Container>
     )
+  }
+}
+
+class Progress extends Component {
+  state = {
+    progress: 0
+  }
+
+  componentDidMount() {
+    this.props.emitter.on(({progress}) => {
+      this.setState({
+        progress: progress
+      });
+    });
+  }
+  Bar = Style.div`
+    position:absolute;
+    left: 0;
+    top: 0;
+    height: 2px;
+    background-color: #337ab7;
+  `
+  render() {
+    return (this.state.progress !== 100) ? <this.Bar style={{width: `${this.state.progress}%`}} /> : <div/>;
+  }
+}
+
+class Options extends Component {
+
+  state = {
+    optionVisible: false
+  }
+
+  OptionsContainer = Style.div`
+    position:absolute;
+    top: 0;
+    right: 0;
+  `
+
+  StyledSettingsButton = Style.button`
+    background: transparent;
+    border: none;
+    cursor: pointer;
+    position: absolute;
+    right: 1rem;
+    top: 1rem;
+    &:focus {
+      outline: none;
+    }
+  `
+  OptionPanel = Style.div`
+    background-color: white;
+    position: absolute;
+    top: 4rem;
+    right: 1rem;
+    padding: 0.6rem;
+    box-shadow: 0px 1px 2px rgba(33, 33, 33, 0.2);
+  `
+
+  fontStyle = `font-family: "Helvetica Neue", Helvetica; font-size: 0.8rem;`;
+  StyledInput = Style.input`
+    ${this.fontStyle}
+    padding: 0.4rem;
+    color: #777;
+  `
+
+  StyledSelect = Style.select`
+    ${this.fontStyle}
+    display: block;
+    width: 100%;
+    padding: 0.4rem;
+    color: #777;
+  `
+
+  StyledLabel = Style.label`
+    ${this.fontStyle}
+    color: #777;
+    display: block;
+    margin-bottom: 1rem;
+  `
+
+  StyledButton = Style.button`
+    ${this.fontStyle}
+    background-color: #5cb85c;
+    color: #f0f0f0;
+    border: none;
+    padding: 0.4rem 1.2rem;
+    cursor: pointer;
+    &:hover {
+      background-color: #61bd61;
+    }
+  `
+
+  ApplyContainer = Style.div`
+    display: flex;
+    justify-content: flex-end;
+    padding: 0.6rem 0;
+  `
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      ...nextProps
+    });
+  }
+
+  handleOptionToggle = () => {
+    this.setState({
+      optionVisible: !this.state.optionVisible
+    });
+  }
+
+  handleTileSizeChange = ({target: {value}}) => {
+    this.setState({
+      tileSize: Math.min(100, Math.max(0, value))
+    });
+  }
+
+  handleOnApply = () => {
+    this.props.onChange(this.state);
+  }
+
+  handleRendererChange = ({target: {value}}) => {
+    this.setState({
+      selectedRenderer: value
+    });
+  }
+  render() {
+    const options = this.state.optionVisible ? (
+      <this.OptionPanel>
+        <this.StyledLabel>
+          Tile Size
+          <this.StyledInput type="number" onChange={this.handleTileSizeChange} value={this.state.tileSize} />
+        </this.StyledLabel>
+        <this.StyledLabel>
+          Renderer
+          <this.StyledSelect onChange={this.handleRendererChange} value={this.state.selectedRenderer}>
+            <option value='default'>Square (default)</option>
+            {this.props.renderers.map( (renderer, index) => {
+              return <option key={index}>{renderer}</option>
+            })}
+          </this.StyledSelect>
+        </this.StyledLabel>
+        <this.ApplyContainer>
+          <this.StyledButton onClick={this.handleOnApply}>Apply</this.StyledButton>
+        </this.ApplyContainer>
+      </this.OptionPanel>
+    ) : '';
+    return (
+      <this.OptionsContainer>
+        <this.StyledSettingsButton onClick={this.handleOptionToggle}>
+          <img src={settingsIcons} />
+        </this.StyledSettingsButton>
+        {options}
+      </this.OptionsContainer>
+    );
   }
 }
 
--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -20,6 +20,13 @@ const config = {
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+          'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
       }
     ]
   },
