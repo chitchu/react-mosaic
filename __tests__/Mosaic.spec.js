8b2acfa66e00ff07916295859d79c31b9a7e4625
Vicente Jr Yuchitcho <chitchu@gmail.com>
2017-02-08 16:35:07 +1100

    :rotating_light: Fix tests

--- __tests__/Mosaic.spec.js
+++ __tests__/Mosaic.spec.js
@@ -5,33 +5,14 @@ import Mosaic from '../src/Mosaic';
 // 10x10 spacer.gif
 const image = 'data:image/gif;base64,R0lGODlhCgAKAIAAAAAAAAAAACH5BAEAAAAALAAAAAAKAAoAAAIIhI+py+0PYysAOw==';
 
-// describe('Component rendering', () => {
-//   it('should contain the correct props', () => {
-//     const wrapper = shallow(
-//       <Mosaic src={image} tileSize={25} width={100} height={100} />
-//     );
-//     const {width, height} = wrapper.props();
-//     expect(wrapper.is('svg')).toBe(true);
-//     expect(width).toBe(100);
-//     expect(height).toBe(100);
-//   });
-// });
-describe('Mount', () => {
-  const promise = new Promise((resolve, reject) => {
-    const wrapper = mount(
-      <Mosaic
-        src={image}
-        tileSize={25}
-        width={100}
-        height={100}
-        onComplete={resolve}
-      />
+describe('Component rendering', () => {
+  it('should contain the correct props', () => {
+    const wrapper = shallow(
+      <Mosaic src={image} tileSize={25} width={100} height={100} />
     );
+    const {width, height} = wrapper.props();
+    expect(wrapper.is('svg')).toBe(true);
+    expect(width).toBe(100);
+    expect(height).toBe(100);
   });
-  return promise
-    .then(() => {
-      expect(true).toBe(false);
-    })
-    .catch(() => {
-    });
 });
