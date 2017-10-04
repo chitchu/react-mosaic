b3cbb218914a2f23f1f671670b05cdefcc913087
chitchu <chitchu@gmail.com>
2017-10-04 15:58:54 +1100

    Tests updated

--- __tests__/Mosaic.spec.js
+++ __tests__/Mosaic.spec.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import {shallow, mount} from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import Mosaic from '../src/Mosaic';
 
+Enzyme.configure({ adapter: new Adapter() });
+
 // 10x10 spacer.gif
-const image = 'data:image/gif;base64,R0lGODlhCgAKAIAAAAAAAAAAACH5BAEAAAAALAAAAAAKAAoAAAIIhI+py+0PYysAOw==';
+const image =
+  'data:image/gif;base64,R0lGODlhCgAKAIAAAAAAAAAAACH5BAEAAAAALAAAAAAKAAoAAAIIhI+py+0PYysAOw==';
 
 describe('Component rendering', () => {
   it('should contain the correct props', () => {
     const wrapper = shallow(
       <Mosaic src={image} tileSize={25} width={100} height={100} />
     );
-    const {width, height} = wrapper.props();
+    const { width, height } = wrapper.props();
     expect(wrapper.is('svg')).toBe(true);
     expect(width).toBe(100);
     expect(height).toBe(100);
